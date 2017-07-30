94027309e6263fed52db8e2222f7a325ca530061
ShangYue <shang0001@ShangYuedeMacBook-Pro.local>
2017-07-30 10:11:06 -0400

    Add map view

---
+++ DealShot/MapViewController.swift
@@ -0,0 +1,56 @@
+//
+//  MapViewController.swift
+//  DealShot
+//
+//  Created by ShangYue on 30/07/2017.
+//  Copyright © 2017 KY. All rights reserved.
+//
+
+import UIKit
+import MapKit
+import CoreLocation
+
+class MapViewController: UIViewController, CLLocationManagerDelegate {
+
+    @IBOutlet var map: MKMapView!
+    let manager: CLLocationManager = CLLocationManager()
+   
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        let location = locations[0]
+        
+        let span:MKCoordinateSpan = MKCoordinateSpanMake(0.01, 0.01)
+        let myLocation:CLLocationCoordinate2D = CLLocationCoordinate2DMake(location.coordinate.latitude, location.coordinate.longitude)
+        let region:MKCoordinateRegion = MKCoordinateRegionMake(myLocation, span)
+        map.setRegion(region, animated: true)
+    
+        self.map.showsUserLocation = true
+    }
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        manager.delegate = self
+        manager.desiredAccuracy = kCLLocationAccuracyBest
+        manager.requestWhenInUseAuthorization()
+        manager.startUpdatingLocation()
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
